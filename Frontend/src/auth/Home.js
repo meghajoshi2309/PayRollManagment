d38cf630654a76323bde63f170ab805d550d5499
meghajoshi2309 <meghajoshi556@gmail.com>
2023-03-31 18:11:49 +0530

    Home page channge

--- Frontend/src/auth/Home.js
+++ Frontend/src/auth/Home.js
@@ -6,12 +6,11 @@ export default function Home() {
     <>
     <Navbar activeLink = {'Home'}/>
     <div  className="card mb-3">
-    <img height="500" width="100%" src="https://www.ramco.com/hubfs/Data-Will-Help-Add-the-Payroll-Function-to-the-Businesses.jpg"  className="card-img-top" alt="..."/>
     <div  className="card-body">
-      <h5  className="card-title">Card title</h5>
-      <p  className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p  className="card-text"><small  className="text-muted">Last updated 3 mins ago</small></p>
+      <h2  className="card-title"><center>Welcome To Payroll Managment</center></h2>
     </div>
+    <img height="500" width="100%" src="https://perkpayroll.com/images/payroll-management-system.png" className="card-img-top" alt="..."/>
+    
   </div>
     </>
   )
